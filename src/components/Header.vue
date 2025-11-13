a8a3e0edb01c03bc8c4bdf4cd04225d066315752
Emi <emiracz05@email.com>
2025-11-13 06:26:05 +0200

    Am adaugat la 2 elemnete tailwindcss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,8 @@ const handleLanguageChange = (lang) => {
 
 <template>
   <header>
-    <h1>EchoFree</h1>
-    <h1 class="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
+
+    <h1 class="text-3xl font-bold text-blue-500">EchoFree</h1>
 
  
     <Navbar/>
@@ -17,3 +17,7 @@ const handleLanguageChange = (lang) => {
     
   </header>
 </template>
+
+<style>
+ @import "tailwindcss";
+</style>
