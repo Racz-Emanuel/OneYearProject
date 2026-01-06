5e5223db73d1ab9f6a0a1b65ceac9d7930add1cc
Emanuel <emiracz05@gmail.com>
2026-01-06 18:05:15 +0200

    Am adaugat integrari pentru ESLint Pinia si Prettier(partial)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import router from './router'
 import './assets/main.css'
-createApp(App).use(router).mount('#app')
+
+const pinia =createPinia()
+const app =createApp(App)
+
+app.use(pinia).use(router).mount('#app')
