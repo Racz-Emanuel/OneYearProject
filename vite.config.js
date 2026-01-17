f627e239679fea28c2d656da27443a1044147403
Emanuel <emiracz05@gmail.com>
2026-01-17 17:47:01 +0200

    Am adaugat PWA

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,41 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
+import tailwindcss from "@tailwindcss/vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  base: '',
-  define:{
+  base: "",
+  define: {
     __VUE_OPTIONS_API__: false
   },
   plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        theme_color: "#169bcb",
+        icons: [
+          {
+            src: "icons/hearing.png",
+            sizes: "192x192",
+            type: "image/png"
+          }
+        ]
+      }
+    }),
     vue(),
     vueDevTools(),
-    tailwindcss(),
-
+    tailwindcss()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 })
